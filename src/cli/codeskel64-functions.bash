b225a0c1ae94f26668423ed2e2e24af937bef1af
serdigital64 <serdigital64@gmail.com>
2022-06-17 10:07:47 -0300

    refact: optimize code

--- src/cli/codeskel64-functions.bash
+++ src/cli/codeskel64-functions.bash
@@ -13,7 +13,6 @@ function codeskel64_list() {
 }
 
 function codeskel64_create_combo() {
-
   local project="$1"
   local target="$2"
   local overwrite="$3"
@@ -29,7 +28,7 @@ function codeskel64_create_combo() {
     if [[ "${combo[0]}" == "$source" ]]; then
       found=0
       unset IFS
-      codeskel64_dispatch \
+      codeskel64_deploy \
         "$overwrite" \
         "${combo[1]}" \
         "${combo[2]}" \
@@ -38,9 +37,37 @@ function codeskel64_create_combo() {
       IFS=':'
     fi
   done < <(bl64_xsv_dump "$catalog")
-  ((found == 1)) && bl64_msg_show_error "undefined combo: [$source]"
+  ((found == 1)) && bl64_msg_show_error "combo not found: [$source]"
 
   return $found
+}
+
+function codeskel64_create_directory() {
+  local project="$1"
+  local target="$2"
+  local overwrite="$3"
+  local source="$4"
+  local destination=''
+
+  bl64_check_directory "$source" || return 1
+
+  if [[ "$target" == "$CODESKEL64_FLAG_USE_PROJECT" ]]; then
+    destination="${project}"
+  else
+    destination="${project}/${target}"
+  fi
+
+  if [[ -d "$destination" ]]; then
+    [[ "$overwrite" == "$BL64_LIB_VAR_OFF" ]] &&
+      bl64_msg_show_error "target directory is already created ($destination). Use -w flag to allow content overwrite" &&
+      return 1
+  else
+    bl64_fs_create_dir "$BL64_LIB_DEFAULT" "$BL64_LIB_DEFAULT" "$BL64_LIB_DEFAULT" "$project" &&
+      bl64_fs_create_dir "$BL64_LIB_DEFAULT" "$BL64_LIB_DEFAULT" "$BL64_LIB_DEFAULT" "$destination"
+  fi
+
+  bl64_fs_merge_dir "$source" "$destination"
+
 }
 
 function codeskel64_create_file() {
@@ -52,12 +79,14 @@ function codeskel64_create_file() {
 
   bl64_check_file "$source" || return 1
   [[ "$overwrite" == "$BL64_LIB_VAR_OFF" && -f "$destination" ]] &&
-    bl64_msg_show_error "target file is already created ($destination)" && return 1
+    bl64_msg_show_error "target file is already created ($destination). Use -w flag to allow content overwrite" && return 1
 
-  bl64_fs_cp_file "$source" "$destination"
+  bl64_fs_create_dir "$BL64_LIB_DEFAULT" "$BL64_LIB_DEFAULT" "$BL64_LIB_DEFAULT" "$project" &&
+    bl64_fs_cp_file "$source" "$destination"
+
 }
 
-function codeskel64_dispatch() {
+function codeskel64_deploy() {
   local overwrite="$1"
   local collection="$2"
   local skeleton="$3"
@@ -71,7 +100,7 @@ function codeskel64_dispatch() {
       "${collection}${BL64_XSV_FS}${skeleton}" \
       "${CODESKEL64_LIBRARY}/${CODESKEL64_PATH_INVENTORY}" \
       "${CODESKEL64_DB_COLLECTION}${BL64_XSV_FS_COLON}${CODESKEL64_DB_SKELETON}" \
-      "${CODESKEL64_DB_TYPE}${BL64_XSV_FS_COLON}${CODESKEL64_DB_SOURCE}" \
+      "${CODESKEL64_DB_TYPE}${BL64_XSV_FS_COLON}${CODESKEL64_DB_DEFAULT}" \
       "$BL64_XSV_FS_COLON" \
       "$BL64_XSV_FS_SPACE"
   )
@@ -79,23 +108,31 @@ function codeskel64_dispatch() {
 
   if [[ "${spec[0]}" == "$CODESKEL64_TYPE_FILE" ]]; then
 
+    [[ "$project" == "$BL64_LIB_DEFAULT" ]] && project="$(pwd)"
     [[ "$target" == "$BL64_LIB_DEFAULT" ]] && target="${spec[1]}"
+    bl64_msg_show_task "create new file (${project}/${target})"
     source="${CODESKEL64_LIBRARY}/${collection}/${CODESKEL64_PATH_SKELETONS}/${skeleton}/${spec[1]}"
-
-    bl64_msg_show_task "create new file using the [${collection}/${skeleton}] skeleton"
     codeskel64_create_file "$project" "$target" "$overwrite" "$source"
 
   elif [[ "${spec[0]}" == "$CODESKEL64_TYPE_DIR" ]]; then
 
     [[ "$target" == "$BL64_LIB_DEFAULT" ]] && target="${spec[1]}"
-    source="${CODESKEL64_LIBRARY}/${collection}/${CODESKEL64_PATH_SKELETONS}/${skeleton}"
 
-    bl64_msg_show_task "create new structure using the [${collection}/${skeleton}] skeleton"
-    bl64_fs_merge_dir "$source" "$project"
+    if [[ "$target" == "$CODESKEL64_FLAG_USE_PROJECT" ]]; then
+      bl64_check_parameter 'project' 'the selected skeleton requires a project path. Retry using the -t paremeter.' || return $?
+      bl64_msg_show_task "create new directory structure (${project})"
+    else
+      bl64_msg_show_task "create new directory structure (${project}/${target})"
+      [[ "$project" == "$BL64_LIB_DEFAULT" ]] && project="$(pwd)"
+    fi
+
+    source="${CODESKEL64_LIBRARY}/${collection}/${CODESKEL64_PATH_SKELETONS}/${skeleton}"
+    codeskel64_create_directory "$project" "$target" "$overwrite" "$source"
 
   elif [[ "${spec[0]}" == "$CODESKEL64_TYPE_COMBO" ]]; then
 
-    bl64_msg_show_task "create new project structure using the [${skeleton}] combo"
+    [[ "$project" == "$BL64_LIB_DEFAULT" ]] && project="$(pwd)/new-${skeleton}-project"
+    bl64_msg_show_task "create new project structure (${project})"
     source="${skeleton}"
     codeskel64_create_combo "$project" "$target" "$overwrite" "$source"
 
@@ -117,18 +154,9 @@ function codeskel64_create() {
 
   bl64_check_parameter 'collection' &&
     bl64_check_parameter 'skeleton' &&
-    bl64_check_parameter 'project' &&
     bl64_check_file "${CODESKEL64_LIBRARY}/${CODESKEL64_PATH_INVENTORY}" || return $?
 
-  if [[ -d "$project" ]]; then
-    [[ "$overwrite" == "$BL64_LIB_VAR_OFF" ]] &&
-      bl64_msg_show_error "project is already created ($project)" &&
-      return 1
-  else
-    bl64_fs_create_dir "$BL64_LIB_DEFAULT" "$BL64_LIB_DEFAULT" "$BL64_LIB_DEFAULT" "$project" || return $?
-  fi
-
-  codeskel64_dispatch \
+  codeskel64_deploy \
     "$overwrite" \
     "$collection" \
     "$skeleton" \
@@ -146,16 +174,16 @@ function codeskel64_help() {
     '<-d|-l|-c> [-w] [-o Collection] [-k Skeleton] [-t Project] [-g Target] [-a Library] [-h]' \
     'Create initial structure from skeletons and templates' \
     '
+    -c           : Create initial code structure
     -d           : Download catalog to LIBRARY
     -l           : List collection and skeletons from LIBRARY
-    -c           : Create initial code structure
     ' '
     -w           : Overwrite target
     -h           : Show help
     ' "
-    -o Collection: Collection name
-    -k Skeleton  : Skeleton name
-    -t Project   : Destination full path
+    -t Project   : Full path to the project where artifacts will be created. Default: <CURRENT_DIRECTORY>
+    -o Collection: Collection name. Use the -l command to show available.
+    -k Skeleton  : Skeleton name. Use the -l command to show available.
     -g Target    : New structure name. Default: skeleton's default
     -a Library   : Library location. Default: XDG_DATA_HOME/codeskel64
     "
